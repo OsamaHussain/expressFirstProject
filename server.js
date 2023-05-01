1ea698c00e23224e08a772b39ebef8abcfaa0820
OsamaHussain <osamahussain897@gmail.com>
2023-05-02 03:45:25 +0500

    Fourth Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { log } = require('console');
 const app = express();
 const port = 3000;
 
@@ -19,51 +20,43 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/about/:name', (req, res) => {
-    res.send(`Welcome! ${req.params.name.charAt(0).toUpperCase()}${req.params.name.substring(1).toLowerCase()} to Our Site`);
 
+  fs.readFile("./employee.json", "utf8", (err, jsonString) => {
 
-    // I want to display my employee details when someone put his/her name in url like
-    // localhost/about/Osama
-    // So the data should be display like image of the user, his name, cnic number, father name, about, interests, and these type of more details.
-    // if user not exist show 404 error on screen...
-    // I have created a sepreate file for this in JSON format where data can be fetch and insert into html page.
+    var data = null;
+    const newJson = JSON.parse(jsonString);
+    for(var i = 0; i < newJson.length; i++){
+      if(newJson[i].name.toLowerCase() == req.params.name.toLocaleLowerCase()){
+        data = newJson[i];
+      }
+    }
+    if(data == null){
+      return res.status(404).sendFile('/404.html', publicDirectoryPath);
+    }else {
+      res.render(__dirname  + '/public/Employee.html', {data : data});
+    }
+    // res.send(data);
+  });
+});
 
+app.get('/about/json/:name', (req, res) => {
 
-    // const name = req.params.name.toLowerCase();
-    // const filePath = path.join(__dirname, 'employee.json');
-    // console.log(__dirname);
-    // // const filePath = publicDirectoryPath + 'employee.json';
+  fs.readFile("./employee.json", "utf8", (err, jsonString) => {
 
-    // fs.readFile(filePath, 'utf8', (err, data) => {
-    //   try {
-    //     const jsonData = JSON.parse(data);
-    //     const employee = jsonData.find(e => e.name.toLowerCase() === name);
-    //     if (!employee) {
-    //       res.status(404).send(`No employee found with the name "${name}"`);
-    //       return;
-    //     }
-    //     const fileName = 'Employee.html';
-    //     const options = { 
-    //       root: path.join(__dirname, 'public'),
-    //     };
-    //     res.sendFile(fileName, options, (err) => {
-    //       if (err) {
-    //         console.error(err);
-    //         res.status(500).send('An error occurred while sending the file');
-    //       }
-    //     });
-    //   } catch (e) {
-    //     console.error(e);
-    //     res.status(500).send('An error occurred while parsing the data');
-    //   }
-    // });
-
-
-
-    // res.sendFile('Employee.html', publicDirectoryPath);
-    // res.sendFile();
-    
-    // res.send(`Welcome! ${req.params.name.charAt(0).toUpperCase()}${req.params.name.substring(1).toLowerCase()} to Our Site`);
+    var data = null;
+    const newJson = JSON.parse(jsonString);
+    for(var i = 0; i < newJson.length; i++){
+      if(newJson[i].name.toLowerCase() == req.params.name.toLocaleLowerCase()){
+        data = newJson[i];
+      }
+    }
+    if(data == null){
+      return res.status(404).sendFile('/404.html', publicDirectoryPath);
+    }else {
+      res.send(data);
+    }
+    ;
+  });
 });
 
 app.listen(port, () => {
